c437f4830fb5e2b3438196ed2302826bbc1e0598
raghav_tiwari <raghavtiwari7151@gmail.com>
2022-04-23 08:24:38 +0530

    schema updation

---
+++ projbackend/models/order.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+//more than one schema in one file not a good call but sometimes needed
+const ProductCartSchema = new mongoose.Schema({
+    product: {
+        type: ObjectId,
+        ref: "Product"
+    },
+    name: String,
+    count: Number,
+    price: Number
+});
+
+const ProductCart = mongoose.model("ProductCart", ProductCartSchema);
+//----------------------------------------------------------------------------------
+const OrderSchema = new mongoose.Schema({
+    products: [ProductCartSchema],
+    transaction_id: {},
+    amount: {type: Number},
+    address: String,
+    update: Date,
+    user: {
+        type: ObjectId,
+        ref: "User"
+    }
+}, {timestamps: true});
+
+
+const Order = mongoose.model("Order", OrderSchema);
+
+
+//external use
+module.exports = {Order,ProductCart};
