34a36f6970d58e89cb9a43a6956bab4220f98dfa
raghav_tiwari <raghavtiwari7151@gmail.com>
2022-04-27 12:04:23 +0530

    updation, deletion , read routes(and controllers)  for are added

---
+++ projbackend/controllers/product.js
@@ -0,0 +1,136 @@
+const Product = require("../models/product");
+const formidable = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
+
+exports.getProductById = (req, res, next, id) => {
+  Product.findById(id)
+    .populate("category")
+    .exec((err, product) => {
+      if (err) {
+        return res.status(400).json({
+          error: "Product not found",
+        });
+      }
+      req.product = product;
+      next();
+    });
+};
+
+//create product
+
+exports.createProduct = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Problem with image",
+      });
+    }
+    //destructure the fields
+    const { name, description, category, price, stock } = fields;
+    if (!name || !description || !price || !category || !stock) {
+      return res.status(400).json({
+        error: "Please include all fields",
+      });
+    }
+
+    let product = new Product(fields);
+
+    //handle file here
+    if (file.photo) {
+      if (file.photo.size > 3000000) {
+        return res.status(400).json({
+          error: "file size too big!",
+        });
+      }
+      product.photo.data = fs.readFileSync(file.photo.path);
+      product.photo.contentType = file.photo.type;
+    }
+    //console.log(product) ---> important step for debugging
+
+    //save to the DB
+    product.save((err, product) => {
+      if (err) {
+        res.status(400).json({
+          error: "saving tshirt in DB failed",
+        });
+      }
+      res.json(product);
+    });
+  });
+};
+
+exports.getProduct = (req, res) => {
+  req.product.photo = undefined;
+  return res.json(req.product);
+};
+
+//middleware for performance optimization
+exports.photo = (req, res, next) => {
+  if ((req, product.photo.data)) {
+    res.set("Content-Type", req.product.photo.contentType);
+    return res.send(req.product.photo.data);
+  }
+  next();
+};
+
+//delete controllers
+exports.deleteProduct = (req, res) => {
+  let product = req.product;
+  product.remove((err, deletedProduct) => {
+    if (err) {
+      return (
+        res.status(400),
+        json({
+          error: "failed to delete the product",
+        })
+      );
+    }
+    res.json({
+      message: "deletewas a success",
+      deletedProduct,
+    });
+  });
+};
+
+//update controllers
+exports.updateProduct = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Problem with image",
+      });
+    }
+   
+    //updation code
+    let product = req.product;
+    product = _.extend(product, fields)
+
+    //handle file here
+    if (file.photo) {
+      if (file.photo.size > 3000000) {
+        return res.status(400).json({
+          error: "file size too big!",
+        });
+      }
+      product.photo.data = fs.readFileSync(file.photo.path);
+      product.photo.contentType = file.photo.type;
+    }
+
+    //save to DB
+    product.save((err, product) => {
+      if (err) {
+        res.status(400).json({
+          error: "udation of product failed",
+        });
+      }
+      res.json(product);
+    });
+  });
+};
