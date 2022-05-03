903d843008c410e0c030748e41323cde14d578f8
raghav_tiwari <raghavtiwari7151@gmail.com>
2022-05-03 08:59:08 +0530

    frontend start

--- testbackend/index.js
+++ testbackend/index.js
@@ -1,10 +1,3 @@
-const express = require("express");
-
-const app = express();
-
-const port = 8000;
-
-
 /*
 REQUESTS:
 
@@ -13,14 +6,34 @@ POST -> send some data to from front end or data  comming from frontend
 PUT -> to update existing values
 DELETE ->
 */
-app.get("/login", (req,res) => {
-    return res.send("hello world you are or login route");
+
+
+const express = require("express");
+
+const app = express();
+
+const port = 7000;
+
+
+
+const admin = (req, res) => {
+  return res.send("THIS IS ADMIN dashboard");
+};
+const isAdmin = (req, res, next) => {
+    return res.send(" ISADMIN running");
+    next();
+  };
+app.get("/admin",isAdmin, admin);
+
+
+app.get("/login", (req, res) => {
+  return res.send("hello world you are or login route");
 });
 
-app.get("/signup", (req,res) => {
-    return res.send("hello world you are or signup route");
+app.get("/signup", (req, res) => {
+  return res.send("hello world you are or signup route");
 });
 
 app.listen(port, () => {
-    console.log("server is up and running  ... ")
+  console.log("server is up and running  ... ");
 });
